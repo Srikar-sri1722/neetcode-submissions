edd8c001614183855d814f7dd24cc8df923c4983
Kandula srikar <kandulasrikar172@gmail.com>
2026-08-25 10:52:06 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int trap(int[] height) {
+        int l=0;
+        int r=height.length-1;
+        int lmax=0;
+        int rmax=0;
+        int res=0;
+        while(l<r){
+            lmax=Math.max(lmax,height[l]);
+            rmax=Math.max(rmax,height[r]);
+            if(lmax<rmax){
+                res+=lmax-height[l];
+                l++;
+            }else{
+                res+=rmax-height[r];
+                r--;
+            }
+        }
+        return res;
+    }
+}
